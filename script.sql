39b05d3fdffa99ad190b1541d0fc5f4ad04df8b3
BrantIvey <bivey10@gmail.com>
2020-01-30 20:13:52 -0600

    q2

--- script.sql
+++ script.sql
@@ -1,6 +1,22 @@
 /*
+Q1. What range of years does the provided database cover?
+
 SELECT MIN(yearid) as first_season, MAX(yearid) as last_season, MAX(yearid)-MIN(yearid) as year_range
 FROM appearances
 
 Answer - 145 years
+
+
+Q2. Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
+
+SELECT CONCAT(p.namefirst,' ', p.namelast), p.height as height, a.g_all as games, a.teamid as team
+FROM people as p
+	JOIN appearances as a
+		ON p.playerid = a.playerid
+ORDER BY height ASC
+LIMIT (1);
+
+Answer - Eddie Gaedel, 43", 1 game with SLA
 */
+
+
