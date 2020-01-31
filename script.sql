21c4e1e0b97477a711daf9bb5c999becc6586dad
BrantIvey <bivey10@gmail.com>
2020-01-30 21:05:02 -0600

    q3

--- script.sql
+++ script.sql
@@ -17,6 +17,30 @@ ORDER BY height ASC
 LIMIT (1);
 
 Answer - Eddie Gaedel, 43", 1 game with SLA
+
+
+Q3. Find all players in the database who played at Vanderbilt University. Create a list showing each player’s first and last names 
+as well as the total salary they earned in the major leagues. Sort this list in descending order by the total salary earned. Which 
+Vanderbilt player earned the most money in the majors?
+
+SELECT concat(p.namefirst,' ',p.namelast) as name, SUM(salaries.salary) as salary, c.schoolid as school
+FROM people as p
+	JOIN collegeplaying as c
+		on p.playerid = c.playerid
+	JOIN salaries
+		on p.playerid = salaries.playerid
+WHERE c.schoolid = 'vandy'
+GROUP BY name, school
+ORDER BY salary DESC;
+
+--Answer: David Price - 245,553,888
+
 */
+
+
+
+
+
+
 
 
